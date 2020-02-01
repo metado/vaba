0f1ade9a4372c43772c81851370230cfa6be0ad5
Anton Parkhomenko <mailbox@chuwy.me>
2020-02-01 17:41:35 +0200

    Feed

--- src/Server.hs
+++ src/Server.hs
@@ -39,14 +39,15 @@ import qualified Text.Blaze.Html
 
 import qualified Data as D
 import qualified Database as DB
-import Config
+import           Config
+import           Feed
 
 type PostAPI = "posts" :> Get '[JSON] [D.Post]
           :<|> "posts" :> ReqBody '[JSON] D.Post :> Post '[JSON] D.ResultMessage
 
 type Aux = "static" :> Raw
 
-type AppAPI = PostAPI :<|> Aux
+type AppAPI = PostAPI :<|> Aux :<|> FeedAPI
 
 data SortBy = PubDate | Title deriving (Eq, Show)
 
@@ -67,7 +68,7 @@ noteServer config = getPosts :<|> postPost
           return $ D.ResultMessage { D.msg = "Thanks!" }
 
 server :: Config -> Server AppAPI
-server config = (noteServer config) :<|> (serveDirectoryWebApp $ staticDir config)
+server config = (noteServer config) :<|> (serveDirectoryWebApp $ staticDir config) :<|> (feedServer config)
 
 app :: Config -> IO Application
 app config = return $ serve appAPI $ server config
