f6b78368f15d06bf90a6f448d4db522e4a3b4435
Anton Parkhomenko <mailbox@chuwy.me>
2020-02-01 20:02:03 +0200

    Update server

--- src/Server.hs
+++ src/Server.hs
@@ -11,29 +11,21 @@
 module Server (PostAPI, noteAPI, app) where
 
 
-import Prelude ()
-import Prelude.Compat
+import           Prelude ()
+import           Prelude.Compat
 
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import Servant.Types.SourceT (source)
+import           Control.Monad.Except
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Attoparsec.ByteString
+import           Data.ByteString (ByteString)
+import           Data.String.Conversions
+import           Data.Time.Calendar
+import           GHC.Generics
+import           Network.HTTP.Media ((//), (/:))
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.Directory
 import qualified Data.Aeson.Parser
 import qualified Text.Blaze.Html
 
