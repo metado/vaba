e7ad35e7cc7ec3265e655f945a52fc8e854f021f
mprushin <mprushinsky@gmail.com>
2020-02-01 14:35:01 +0200

    Use Post typeclass

--- src/Client.hs
+++ src/Client.hs
@@ -1,14 +1,13 @@
-#!/usr/bin/env stack
--- stack script --resolver lts-12.21
 {-# LANGUAGE OverloadedStrings #-}
 
 module Client (client) where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Network.HTTP.Simple
+import           Post
 
-client :: IO ()
-client = do
+feed :: [Post]
+feed = do
     response <- httpLBS "http://httpbin.org/get"
 
     putStrLn $ "The status code was: " ++
